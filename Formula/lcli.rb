4e61784cfc6393c3388d1ced75b9c90fd381406a
Pavel Varganov <varganov.p.p@gmail.com>
2026-03-06 18:59:53 +0400

    lcli v0.1.1

--- Formula/lcli.rb
+++ Formula/lcli.rb
@@ -1,28 +1,28 @@
 class Lcli < Formula
   desc "CLI for Linear issue tracker"
   homepage "https://github.com/pvarganov/lcli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/pvarganov/lcli/releases/download/v0.1.0/lcli_darwin_arm64.tar.gz"
-      sha256 "c7f0624bf6d2738d29efecd6916ad911aa9f751a74dd9cd5547a9499b3557af0"
+      url "https://github.com/pvarganov/lcli/releases/download/v0.1.1/lcli_darwin_arm64.tar.gz"
+      sha256 "5edc9ca1f281174263c3a1b7070475aa07bc4eb2afc644b7848ff75becd633ca"
     end
     on_intel do
-      url "https://github.com/pvarganov/lcli/releases/download/v0.1.0/lcli_darwin_amd64.tar.gz"
-      sha256 "018c8735ef0976f39e1a9601d395183e9c31c31108869330bad7519b614735de"
+      url "https://github.com/pvarganov/lcli/releases/download/v0.1.1/lcli_darwin_amd64.tar.gz"
+      sha256 "193723eb924d654e3a233251cae48eba6a23ff97aa361879ca700944a50b056d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pvarganov/lcli/releases/download/v0.1.0/lcli_linux_arm64.tar.gz"
-      sha256 "82caa98c17c63767784c0338c8ddcb96ca788047298a5eb99221648be4dc4cd9"
+      url "https://github.com/pvarganov/lcli/releases/download/v0.1.1/lcli_linux_arm64.tar.gz"
+      sha256 "fd4925498fd3791ab9756facb93f12721492de3a0f9b65350128cca281fb3779"
     end
     on_intel do
-      url "https://github.com/pvarganov/lcli/releases/download/v0.1.0/lcli_linux_amd64.tar.gz"
-      sha256 "de215d01063acba2114256a607716c63abfc90a379ac0a72dd483c0e047dfec3"
+      url "https://github.com/pvarganov/lcli/releases/download/v0.1.1/lcli_linux_amd64.tar.gz"
+      sha256 "421f61b7688a4f923e410d3304d0e4becde107856ec8ca6f9874f23f0941c742"
     end
   end
 
